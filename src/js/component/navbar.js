d683c7a71ac659e932d232fff3bfdd8ef9a58a0b
edoalemp <eduardo.munoz.p@gmail.com>
2019-07-21 02:25:43 +0000

    creando home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export class Navbar extends React.Component {
 	render() {
@@ -7,9 +9,13 @@ export class Navbar extends React.Component {
 			<nav className="navbar navbar-expand-lg navbar-light bg-dark">
 				<div className="dropdown">
 					<button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+						<FontAwesomeIcon icon={faBars} />
 						<i className="fas fa-grip-lines" />
 					</button>
 					<div className="dropdown-menu">
+						<Link className="dropdown-item" to="/">
+							Inicio
+						</Link>
 						<Link className="dropdown-item" to="/estaciones">
 							Estaciones
 						</Link>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,14 +5,47 @@ import "../../styles/home.scss";
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
+			<div className="text-center">
+				<div className="mt-5">
+					<h1> Telemetría </h1>
+				</div>
+
+				<div className="image">
+					<img src="https://www.ibm.com/support/knowledgecenter/es/SSFKSJ_8.0.0/com.ibm.mq.pro.doc/q001030b.gif" />
+				</div>
+
+				<div className="description ml-3">
+					<div className="row">
+						<div className="col-md-11 col-center ml-5 mt-5">
+							<h6> </h6>
+						</div>
+					</div>
+				</div>
+
+				<div className="menuvista1">
+					<div className="row">
+						<div className="col-md-11 col-center text-center ml-5 mt-5">
+							<div className="p-3 mb-2 bg-dark text-white w-25">
+								<h4> Usuario </h4>
+							</div>
+						</div>
+						<div className="col-md-11 col-center text-center ml-5 mt-5">
+							<div className="p-3 mb-2 bg-dark text-white w-25">
+								<h4> Password </h4>
+							</div>
+						</div>
+						<div className="col-md-11 col-center text-center ml-5 mt-5">
+							<div className="p-3 mb-2 bg-dark text-white w-25">
+								<h4> Log In </h4>
+							</div>
+						</div>
+						<div className="col-md-11 col-center text-center ml-5 mt-5">
+							<div className="p-3 mb-5 bg-dark text-white w-25">
+								<h4> Nuevo Usuario </h4>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
