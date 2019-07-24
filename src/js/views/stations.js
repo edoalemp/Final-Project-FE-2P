56ad9d24340827a89e495312e9a05b94208a516f
edoalemp <eduardo.munoz.p@gmail.com>
2019-07-24 01:17:54 +0000

    configurando vistas

--- src/js/views/stations.js
+++ src/js/views/stations.js
@@ -8,32 +8,41 @@ import "../../styles/demo.scss";
 export class Stations extends React.Component {
 	render() {
 		return (
-			<div className="container text-center">
-				<div className="row  justify-content-md-center">
+			<div className="container">
+				<div className="row justify-content-md-center">
 					<div className="col-md-6 col-center mt-1">
-						<div data-spy="scroll" data-target=".navbar" data-offset="50">
-							<nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-								<ul className="navbar-nav">
-									<li className="nav-item">
-										<a className="nav-link" href="#section1">
-											Section 1
-										</a>
-									</li>
-								</ul>
-							</nav>
-
-							<div id="section1" className="container-fluid bg-success">
-								<div className="list-group">
-									<a href="#" className="list-group-item list-group-item-action">
-										First item
-									</a>
-									<a href="#" className="list-group-item list-group-item-action">
-										Second item
-									</a>
-									<a href="#" className="list-group-item list-group-item-action">
-										Third item
-									</a>
-								</div>
+						<nav id="navbarint" className="navbar navbar-light bg-light">
+							<a className="navbar-brand" href="#liststations">
+								Estaciones
+							</a>
+						</nav>
+						<div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
+							<div className="list-group" id="liststations">
+								<a
+									href="#"
+									className="list-group-item list-group-item-action m-0 p-0 border border-0 text-left">
+									Cras justo odio
+								</a>
+								<a
+									href="#"
+									className="list-group-item list-group-item-action m-0 p-0 border border-0  text-left">
+									Dapibus ac facilisis in
+								</a>
+								<a
+									href="#"
+									className="list-group-item list-group-item-action m-0 p-0 border border-0 text-left">
+									Morbi leo risus
+								</a>
+								<a
+									href="#"
+									className="list-group-item list-group-item-action m-0 p-0 border border-0 text-left">
+									Porta ac consectetur ac
+								</a>
+								<a
+									href="#"
+									className="list-group-item list-group-item-action m-0 p-0 border border-0 text-left ">
+									Vestibulum at eros
+								</a>
 							</div>
 						</div>
 					</div>
